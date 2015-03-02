919a959ee9a7a760d55fc76929a9a1f26990a2d7
Koushan <kooshanfilm@hotmail.com>
2015-03-01 22:06:43 -0800

    Els and if

---
+++ else.rb
@@ -0,0 +1,28 @@
+#else
+
+print "Enter your name"
+name = gets.chomp
+
+if name == "james"
+	puts "hello man"
+else
+	puts "Hi #{name}"
+end
+
+print "if you like to see your name reverse"
+
+ans = gets.chomp
+
+if ans == "yes"
+	puts ans.name
+
+elsif ans = "not now"
+	puts "Ummmmm"
+
+else
+	puts "why not "
+
+end
+
+
+
---
+++ if_and_else.rb
@@ -0,0 +1,16 @@
+#learn if and else
+
+car1 = 500
+car2 = 300
+
+if car1 > car2 
+	puts "Car1 is faster"
+end
+
+
+car2 += 400
+
+if car2 > car1 
+	puts "car2 is faster than car1"
+end
+

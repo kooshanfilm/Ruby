f612b36667cef271a5feba5986bb9a2f2a49e11d
Koushan <kooshanfilm@hotmail.com>
2015-03-05 22:03:32 -0800

    Array

---
+++ more_on_array.rb
@@ -0,0 +1,21 @@
+book = ["one","two","three"]
+
+puts book[0]
+
+puts book.at(1)
+
+puts book.first
+
+puts book.last
+
+puts book[-1]
+
+puts book.insert(2,"james")
+
+puts book.length
+
+puts book.count
+
+puts book.count("james")
+
+puts book.include?("james")
---
+++ removing_from_array.rb
@@ -0,0 +1,14 @@
+#removing_from_array
+
+book = ["one","two","three","four","five","six"]
+
+last_item = book.pop
+
+first = book.shift # will remove the fist item from array 
+
+puts first
+puts book.inspect
+
+drop_2 = book.drop(1)
+
+print drop_2

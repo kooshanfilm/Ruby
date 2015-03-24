dcb05fbbfcfb26799a3a065ee69fb647b4c01089
Koushan <kooshanfilm@hotmail.com>
2015-03-23 22:33:56 -0700

    hash.each do |key,value|

---
+++ hash_iteration.rb
@@ -0,0 +1,10 @@
+
+location = {"name" => "Canada", "Time" => "PST"}
+
+location.each do |key,value|
+	puts "my key is #{key} and my value  is #{value}"
+	
+	end
+location.each_key do |key|
+	puts "this is my #{key}"
+end

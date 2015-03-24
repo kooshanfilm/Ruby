cc51fc94b1f471d0bcbcc02b6aa95d61ae385184
Koushan <kooshanfilm@hotmail.com>
2015-03-23 22:33:36 -0700

    variable.each do |name|

---
+++ iteration_with_each.rb
@@ -0,0 +1,13 @@
+
+array = [0,12,213,2123,123]
+
+# i = 0 
+# while i < array.length
+# 	item = array[i]
+# 	puts "the current item is #{item}"
+# 	i +=1
+# end
+
+array.each do |item|
+	 puts "the current item is #{item}" 
+end

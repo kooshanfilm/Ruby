1b2caa0882698649a20065659713e0e57925bec0
Koushan <kooshanfilm@hotmail.com>
2015-03-27 14:41:17 -0700

    All about hash

--- all_about_hash.rb
+++ all_about_hash.rb
@@ -1,47 +1,45 @@
 
 def create_list
+
 	print "what is your list"
 	name = gets.chomp
-
 	hash = {"name" => name , "items" => Array.new }
 	return hash
+
 end
 
 def add_list_item
+
 	print "what is the item called?"
 	item_name = gets.chomp
-
 	print "How much "
 	quantity = gets.chomp.to_i #get.chomp by defult is string 
-
 	hash = {"name" => item_name , "quantity" => quantity}
-
 	return hash
+
 end
 
 def print_seperator(character="")
+
 	puts character * 80
+
 	end
 
-
 def print_list(list)
+
 	puts "List #{list['name']}"
 	print_seperator(-)
-
 	list["items"].each do |item|
 		puts "Item " + item['name']
 		puts "quantity" + item['quantity'].to_s
 		print_seperator(-)
+
 	end
-
-	
 end
-
 
 list = create_list() # will insert the hash into the list 
 list['items'].push(add_list_item)
 puts "My list now #{list.inspect}"
-
 print_list(list)
 
 

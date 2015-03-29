001beff3169a1aacb70866582e5dfc290d186b54
Koushan <kooshanfilm@hotmail.com>
2015-03-28 22:25:19 -0700

    about ruby classes

---
+++ Ruby_class.rb
@@ -0,0 +1,18 @@
+
+string = String.new
+
+puts string.class
+
+jason = String.new("James")
+
+puts jason
+
+#lest say we have the following class
+class MyClass
+  def initialize 
+    puts "whenever we creat a instant of the class this will print."
+  end
+end
+
+MyClass.new
+

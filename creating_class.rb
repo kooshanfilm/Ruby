573e708067a0973617cc75a682b29588f3e18481
Koushan <kooshanfilm@hotmail.com>
2015-04-12 23:35:34 -0700

    how to create a class

--- creating_class.rb
+++ creating_class.rb
@@ -1,19 +1,30 @@
 
 class Name
-	def title
-		"Mr."
+	attr_reader :title, :last_name
+
+	def initialize(title,last_name)
+		# create a instant variable
+		# will be accessable to the rest of class
+		@title = title
+		@last_name = last_name
+	
 	end
+
+
+	# def title
+	# 	return @title
+	# end
 
 	def first_name
 		"James"
 	end
 
-	def last_name
-		""
-	end
+	# def last_name
+	# 	@last_name
+	# end
 
 end
 
- name = Name.new
-
- puts name.title
+ name = Name.new("james","tomas")
+ puts name
+ # puts name.last_name

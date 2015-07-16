228ae8120740693b6ec15730fd047d5027e1e6cc
Koushan <kooshanfilm@hotmail.com>
2015-07-15 22:08:34 -0700

    reversing the array

--- revers_the_array.rb
+++ revers_the_array.rb
@@ -1,19 +1,18 @@
 class Reversing_array
-
-def reverse_me
-
-  my_array = [1,2,3,4,5,6,7]
-  arr_length = my_array.length
-  i = 0
-  j = arr_length - 1
-
-  while arr_length > i
-    puts my_array[j]
-    i +=1
-    j -=1
+  def reverse_me
+    my_array = [0,1,2,3,4,5]
+    arr_length = my_array.length
+    i = 0
+    j = arr_length - 1
+    my_second_array = []
+      while arr_length > i
+        my_second_array.push(my_array[j])
+        i +=1
+        j -=1
+      end
+    return my_second_array
+    end
   end
-end
-end
 
-call_my_class = Reversing_array.new
-call_my_class.reverse_me
+reversing_array = Reversing_array.new()
+puts reversing_array.reverse_me.inspect

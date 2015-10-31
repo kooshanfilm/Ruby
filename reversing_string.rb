388a54510bc84f237fab16de84ba23bdba5800d3
Koushan <kooshanfilm@hotmail.com>
2015-10-31 14:56:26 -0700

    modifiy the function

--- reversing_string.rb
+++ reversing_string.rb
@@ -1,16 +1,17 @@
 def str
-	str = "abcd"
-	sec_str = []
-	string_len = str.length
-	j = string_len -1
-	i = 0 
-	while j>=0
-		sec_str[i] = str[j]
-		#puts sec_str[i]
-		j-=1
-	end
-	return sec_str[2]
-		
-end
+  puts "Please enter the sting you want to be reverse"
+  str = gets.chomp()
+  #str = "test"
+  sec_str = []
+  string_len = str.length
+  j = string_len -1
+  i = 0
+  while j>=0
+    sec_str[i] = str[j]
+    j-=1
+  end
 
+  return sec_str.inspect
+
+end
 puts str
